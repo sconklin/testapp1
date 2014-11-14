8c1829e415ae1f3985eb67ebf4d7094a8464bc6d
Steve Conklin <steve@conklinhouse.com>
2014-11-14 14:39:55 -0600

    Add some seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,6 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 Article.delete_all
+Article.create(title: 'Title00000000', text: 'text 00000000', color: 0)
+Article.create(title: 'Title00000001', text: 'text 00000001', color: 1)
+Article.create(title: 'Title00000002', text: 'text 00000002', color: 2)
